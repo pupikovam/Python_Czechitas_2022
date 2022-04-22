6909d9f3cff4bb1ae8b5c8101ca96aabe199004e
MonikaP <pupikovam@gmail.com>
2022-04-22 16:23:19 +0200

    ukol7

---
+++ ukol7.py
@@ -0,0 +1,12 @@
+with open("posta.html", encoding='utf-8') as vstup:
+    posta = vstup.read()
+
+import re
+
+posta=posta.replace('\n', ' ')
+reg_vyraz_mezery = re.compile(" +")
+posta=re.sub(reg_vyraz_mezery, " ", posta)
+print(posta)
+reg_vyraz_mesta=re.compile("\d{3} \d{2}( [^\d\s!<]+)+( \d+)*")
+mesta=reg_vyraz_mesta.findall(posta)
+print(mesta)
